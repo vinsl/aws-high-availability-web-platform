a57039b8ce02db74826f675815458876068d6ffc
vincent-lucas <tigroulucas@gmail.com>
2026-09-14 10:19:58 +0000

    added ALB

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,13 @@ module "security" {
   environment  = var.environment
   vpc_id       = module.network.vpc_id
 }
+
+module "load_balancer" {
+  source = "./modules/load_balancer"
+
+  project_name          = local.project_name
+  environment           = var.environment
+  vpc_id                = module.network.vpc_id
+  public_subnet_ids     = module.network.public_subnet_ids
+  alb_security_group_id = module.security.alb_security_group_id
+}
---
+++ terraform/modules/load_balancer/main.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${var.project_name}-${var.environment}-app-tg"
+  port        = 5000
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    enabled             = true
+    path                = "/health"
+    protocol            = "HTTP"
+    port                = "5000"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
