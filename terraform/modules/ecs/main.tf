11e0f2961ab102f9206f0fed84718d01fba624b5
vincent-lucas <tigroulucas@gmail.com>
2026-09-14 13:19:47 +0000

    Added ECS variables

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -32,3 +32,60 @@ resource "aws_iam_role_policy_attachment" "task_execution" {
   role       = aws_iam_role.task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.project_name}-${var.environment}-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "support-desk"
+      image     = var.container_image
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 5000
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "DB_HOST"
+          value = var.db_host
+        },
+        {
+          name  = "DB_PORT"
+          value = tostring(var.db_port)
+        },
+        {
+          name  = "DB_NAME"
+          value = var.db_name
+        },
+        {
+          name  = "DB_USER"
+          value = var.db_username
+        },
+        {
+          name  = "DB_PASSWORD"
+          value = var.db_password
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.app.name
+          awslogs-region        = "eu-west-3"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -5,3 +5,43 @@ variable "project_name" {
 variable "environment" {
   type = string
 }
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "app_security_group_id" {
+  type = string
+}
+
+variable "target_group_arn" {
+  type = string
+}
+
+variable "db_host" {
+  type = string
+}
+
+variable "db_port" {
+  type = number
+}
+
+variable "db_name" {
+  type    = string
+  default = "support_desk"
+}
+
+variable "db_username" {
+  type    = string
+  default = "support_desk"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "container_image" {
+  type    = string
+  default = "vinsl/support-desk:1.0.1"
+}
